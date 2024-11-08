93905457f0806082d67b9a8c8e9b3a3a643181e0
Sebastian S <74125017+sebasinmas@users.noreply.github.com>
2024-11-08 13:16:25 -0300

    Hotfix: Arregla redundancia en la función asíncrona

--- routes/index.js
+++ routes/index.js
@@ -5,14 +5,12 @@ const db = require('../config/db'); // Importa la conexión a la base de datos
 router.get('/', async (req, res) => {  // Cuando se accede a la ruta principal
     try {
         const [rows] = await db.query('SELECT * FROM Nombre'); // Consulta la base de datos. Una variable envuelta en corchetes significa que solo se usará la primera fila
-        console.log(rows);
         res.render('index', { nombres: rows }); // Renderiza el archivo "index.html" en la carpeta "views" y envía los nombres a la plantilla
     } catch (error) {
-        console.log(error);
+    
         res.status(500).send('Error en el servidor'); // Si hay un error, envía un mensaje de error al cliente
         //Esto no es necesario, pero es una buena práctica.
     }
-    res.render('index');  // Renderiza el archivo "index.html" en la carpeta "views"
 });
 
 module.exports = router; //Añade el router a la lista de rutas disponibles
